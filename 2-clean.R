f7dfffb04281cc4fdd37147901921b8944966f1f
Geoff <geoff@dropbox.com>
2016-05-23 16:15:53 -0700

    Remove duplicate rows and check for null values in cleaning phase.

--- 2-clean.R
+++ 2-clean.R
@@ -4,4 +4,15 @@ df <- raw %>%
   filter() %>%
   mutate()
 
+# check for rows with null values
+# df[rowSums(is.na(df)) > 0,]
+
+# fill null entries for one column
+# df %<>%
+#   mutate(colname = ifelse(is.na(colname), 0, colname))
+
+# remove duplicate rows
+df %<>%
+  subset(!duplicated(colname))
+
 save(df, file = "cleaned.RData")
